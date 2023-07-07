a5727a840e038b2983c4991e240e63ca414e56e7
hossein <abdollahzadeh.hossein@gmail.com>
2023-07-07 09:48:47 +0330

    Show id, title_fa, title_en fields (instead of show all fields)

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -4,7 +4,7 @@ const movieController = {
     getAll: async (req, res) => {
         try {
             const { page, limit, sort_title_fa } = req.query;
-            let data; // Declare the 'data' variable here
+            let data;
 
             if (page && limit) {
                 const offset = (page - 1) * limit;
@@ -12,13 +12,13 @@ const movieController = {
 
                 if (sort_title_fa) {
                     const sortOrder = sort_title_fa === 'desc' ? 'DESC' : 'ASC';
-                    query = `SELECT * FROM movies ORDER BY CONVERT(title_fa USING utf8mb4) COLLATE utf8mb4_persian_ci ${sortOrder} LIMIT ? OFFSET ?`;
+                    query = `SELECT id, title_fa, title_en FROM movies ORDER BY CONVERT(title_fa USING utf8mb4) COLLATE utf8mb4_persian_ci ${sortOrder} LIMIT ? OFFSET ?`;
                 } else {
-                    query = 'SELECT * FROM movies LIMIT ? OFFSET ?';
+                    query = 'SELECT id, title_fa, title_en FROM movies LIMIT ? OFFSET ?';
                 }
 
                 const [result] = await pool.query(query, [+limit, +offset]);
-                data = result; // Assign the result to 'data'
+                data = result;
 
                 const [totalPageData] = await pool.query('SELECT COUNT(*) AS count FROM movies');
                 const totalPage = Math.ceil(+totalPageData[0]?.count / limit);
@@ -34,19 +34,19 @@ const movieController = {
             } else {
                 if (sort_title_fa) {
                     const sortOrder = sort_title_fa === 'desc' ? 'DESC' : 'ASC';
-                    query = `SELECT * FROM movies ORDER BY CONVERT(title_fa USING utf8mb4) COLLATE utf8mb4_persian_ci ${sortOrder}`;
+                    query = `SELECT id, title_fa, title_en FROM movies ORDER BY CONVERT(title_fa USING utf8mb4) COLLATE utf8mb4_persian_ci ${sortOrder}`;
                 } else {
-                    query = 'SELECT * FROM movies';
+                    query = 'SELECT id, title_fa, title_en FROM movies';
                 }
                 const [result, fields] = await pool.query(query);
-                data = result; // Assign the result to 'data'
+                data = result;
 
                 res.json({
                     data: data,
                     pagination: {
                         page: +page,
                         limit: +limit,
-                        totalPage: 1 // You might want to set a default value for totalPage when 'page' and 'limit' are not provided
+                        totalPage: 1
                     }
                 });
             }
